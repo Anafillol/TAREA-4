fd7cde9829c5f0ac330ca28b59efbbd362cf2f21
Ana <157888601+Anafillol@users.noreply.github.com>
2024-04-26 01:13:43 +0200

    esta es la práctica Junior
    Aqui estoy subiendo mi html, css y javascript

--- script.js
+++ script.js
@@ -1,29 +1,77 @@
-function showImage(imageUrl, selectedTabLinkId) {
-    var largeImage = document.getElementById("largeImage");
-    largeImage.src = imageUrl;
-    largeImage.style.display = "block"; 
-    
-    // cambioo la opacidad
-    var tabContents = document.querySelectorAll(".tab__tabLink");
-    tabContents.forEach(function(tabContent) {
-        var tabLinkId = tabContent.id;
-        var text = tabContent.querySelector(".tab__tabLink");
-        if (tabLinkId !== selectedTabLinkId) {
-            text.style.opacity = "0.3"; 
-        } else {
-            text.style.opacity = "1"; 
-        }
+
+document.addEventListener('DOMContentLoaded', function() {
+    handleEvents();
+    mostrarSilla(); // Asi se ve la sección de la silla al cargar la pagina
+});
+
+// Escribo aqui todas las funciones asi luego llamarlas dentro de handleEvents
+function handleEvents() {
+    let botonAbrir = document.getElementById("boton");
+    let botonCerrar = document.getElementById("botonCerrar");
+    let contenedorDiapositivas = document.getElementById("contenedorDiapositivas");
+    let diapositivas = document.querySelector(".sliderContainer__diapositiva");
+    let collectionButton = document.querySelector('.cabecera__link'); // Botón "Collection"
+    let furnitureButton = document.querySelector('.subcategoria1'); // Botón "Furniture"
+
+    botonAbrir.addEventListener("click", handleToggle);
+    botonCerrar.addEventListener("click", handleToggle);
+
+    let botonDelante = document.getElementById("flechaDelante");
+    let botonAtras = document.getElementById("flechaAtras");
+
+    botonDelante.addEventListener("click", mostrarMesa);
+    botonAtras.addEventListener("click", mostrarSilla);
+
+    // evento clicar en el botón collection
+    collectionButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        toggleExpanded(document.getElementById('dropdownCollection'));
+    });
+
+    // evento clicar en el botón collection furniture
+    furnitureButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        mostrarImagenSofa();
     });
 }
 
-function hideImage() {
-    var largeImage = document.getElementById("largeImage");
-    largeImage.style.display = "none"; // Oculto la imagen grande
-    
-    // vuelvo a cambiar la opacidaad 
-    var tabContents = document.querySelectorAll(".tabContent");
-    tabContents.forEach(function(tabContent) {
-        var text = tabContent.querySelector(".tab__tabLink");
-        text.style.opacity = "1"; 
-    });
+function toggleExpanded(element) {
+    element.classList.toggle('expanded');
+}
+
+function handleToggle() {
+    let contenedor = document.getElementById("contenedorHeader");
+    contenedor.classList.toggle('expanded');
+}
+
+function mostrarSilla() {
+    const silla = document.querySelector('.seccion_detallesSilla');
+    const mesa = document.querySelector('.seccion_detallesMesa');
+
+    if (mesa.classList.contains('active')) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+        silla.classList.add('active');
+        mesa.classList.remove('active');
+        silla.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+
+    setTimeout(function() {
+        silla.classList.add('active');
+    }, 500);
+}
+
+function mostrarMesa() {
+    const silla = document.querySelector('.seccion_detallesSilla');
+    const mesa = document.querySelector('.seccion_detallesMesa');
+
+    silla.classList.remove('active');
+    mesa.classList.add('active');
+
+    mesa.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function mostrarImagenSofa() {
+    const imagenSofa = document.querySelector('.Imagen__sofa');
+    imagenSofa.style.display = 'block';
 }
